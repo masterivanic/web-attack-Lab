58f1000f761b4b3e16c2ab627db87c3d8144b59e
masterivanic <masterivanic@gmail.com>
2025-09-07 03:04:31 +0200

    chore: add custom install lab configuration

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 ## WEB ATTACK LAB
+
+# Vulnerability Support Configuration (PHP Settings)
+
+- To intentionally make the DVWA environment vulnerable and suitable for exploitation practice, specific PHP configurations were modified:
+```sh
+display_errors = On
+```
+
+- Displays error messages directly in the browser, which can leak sensitive details like file paths, code snippets, and system messages useful for attackers.
+
+```sh
+display_startup_errors = On
+```
+
+- Enables displaying of PHP startup sequence errors, offering further environment details during application boot that could aid exploitation.
+
+```sh
+allow_url_include = On
+```
+
+Allows the inclusion of remote files via functions like include() and require(), making Remote File Inclusion (RFI) vulnerabilities possible.
--- install.sh
+++ install.sh
@@ -1,1 +1,39 @@
+#!/bin/bash
+
 ## All attack and utils command here
+source .env
+
+architecture=""
+mysqlImage=""
+
+docker buildx build -t local/kali-machine -f Dockerfile.machine .
+docker run --rm -it -p 80:80 vulnerables/web-dvwa --name vulnerable-container
+
+function getArchitecture(){
+    case $(uname -m) in
+        i386)   architecture="386" ;;
+        i686)   architecture="386" ;;
+        x86_64) architecture="amd64" ;;
+        arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+    esac
+}
+
+function startMysqlDb() {
+    echo "starting mysql container...."
+    docker run --name db-container \
+      -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+      -e MYSQL_USER=${MYSQL_USER} \
+      -e MYSQL_PASSWORD=${MYSQL_USER} \
+      -e MYSQL_DATABASE=${MYSQL_DATABASE} \
+      -d ${mysqlImage}
+}
+
+getArchitecture
+
+if [[ ${architecture} == "arm" ]]; then
+    mysqlImage="mysql:8"
+elif [[ ${architecture} == "x86_64" ]]; then
+    mysqlImage="mysql:8.0.42-bookworm"
+else
+    echo "Unsupported platform"
+fi
